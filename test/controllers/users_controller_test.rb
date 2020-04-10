730519e002ee4313356de50d31f5d51762fdbaaa
YC-Cho <ycho1343@conestogac.on.ca>
2020-04-10 02:16:58 +0000

    Add user following

--- test/controllers/users_controller_test.rb
+++ test/controllers/users_controller_test.rb
@@ -37,7 +37,8 @@ class UsersControllerTest < ActionDispatch::IntegrationTest
     end
     assert_redirected_to login_url
   end
-
+  
+  
   test "should redirect destroy when logged in as a non-admin" do
     log_in_as(@other_user)
     assert_no_difference 'User.count' do
@@ -45,4 +46,16 @@ class UsersControllerTest < ActionDispatch::IntegrationTest
     end
     assert_redirected_to root_url
   end
+  
+  test "should redirect following when not logged in" do
+    get following_user_path(@user)
+    assert_redirected_to login_url
+    
+  end
+
+  test "should redirect followers when not logged in" do
+    get followers_user_path(@user)
+    assert_redirected_to login_url
+  end
+  
 end
